afea5a53d963439113199fd9c397eba7a5c65f08
Frank Lemanschik <frank@github.dspeed.eu>
2017-02-17 21:18:22 +0000

    Update index.js
    Detect if running inside node to prevent error document not defined

--- index.js
+++ index.js
@@ -12,15 +12,22 @@ module.exports = ie();
  */
 
 function ie() {
-  for( var v = 3,
-           el = document.createElement('b'),
-           // empty array as loop breaker (and exception-avoider) for non-IE and IE10+
-           all = el.all || [];
-       // i tag not well-formed since we know that IE5-IE9 won't mind
-       el.innerHTML = '<!--[if gt IE ' + (++v) + ']><i><![endif]-->',
-       all[0];
-     );
-  // return the documentMode for IE10+ compatibility
-  // non-IE will get undefined
-  return v > 4 ? v : document.documentMode;
+  // Detect if running inside NodeJS
+  if(typeof process !== 'object' && process + '' !== '[object process]'){
+    // isNot node
+    for( var v = 3,
+             el = document.createElement('b'),
+             // empty array as loop breaker (and exception-avoider) for non-IE and IE10+
+             all = el.all || [];
+         // i tag not well-formed since we know that IE5-IE9 won't mind
+         el.innerHTML = '<!--[if gt IE ' + (++v) + ']><i><![endif]-->',
+         all[0];
+       );
+    // return the documentMode for IE10+ compatibility
+    // non-IE will get undefined
+    return v > 4 ? v : document.documentMode;  
+  } else {
+    return;
+  }
+  
 }
